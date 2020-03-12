f740bc047c514900f43a6dd057ec549078aef0e6
Fly me to the moon <git@bigly.dog>
2020-03-11 22:36:00 -0700

    added future

--- src/isomorphic/prelude.ts
+++ src/isomorphic/prelude.ts
@@ -2,6 +2,16 @@ export const id = <T>(x: T) => x
 
 export const str = (thing: any): string => thing.toString()
 
+export const future = <T>() => {
+  let resolve: (value: T) => void = (undefined as any) as (err: any) => void
+  let reject: (err: any) => void = (undefined as any) as (err: any) => void
+  const promise = new Promise<T>((res, rej) => {
+    resolve = (value) => res(value)
+    reject = (err) => rej(err)
+  })
+  return { promise, resolve, reject }
+}
+
 export const sleep = (ms: number) =>
   new Promise<void>((resolve) => setTimeout(resolve, ms))
 
