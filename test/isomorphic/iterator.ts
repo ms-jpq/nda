a6d01ebc49fe4d0c726d76e1de3a83c4b67ec4eb
Fly me to the moon <git@bigly.dog>
2020-03-22 02:42:47 -0700

    added more tests

--- test/isomorphic/iterator.ts
+++ test/isomorphic/iterator.ts
@@ -1,5 +1,15 @@
-import { sort_by_keys, range } from "../../src/isomorphic/iterator"
 import assert from "assert"
+import { sort_by_keys, range, drop, take } from "../../src/isomorphic/iterator"
+
+const drop_1 = () => {
+  const lst = [...drop(5, range(1, 10))]
+  assert(lst.length === 5)
+}
+
+const take_1 = () => {
+  const lst = [...take(5, range(1, 10))]
+  assert(lst.length === 5)
+}
 
 const sort_by_keys_1 = () => {
   const set = new Set(range(5, 1, -1))
@@ -12,5 +22,7 @@ const sort_by_keys_1 = () => {
 }
 
 export const run_iterator = async () => {
+  drop_1()
+  take_1()
   sort_by_keys_1()
 }
