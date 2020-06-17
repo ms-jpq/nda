3f70e53e0de57042b4a9b03eb09ce70fc9883da2
Fly me to the moon <git@bigly.dog>
2020-06-17 11:23:01 -0700

    build ts

---
+++ build.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env ts-node
+
+import { call } from "./src/node/sub_process"
+
+const main= async () => {
+  process.chdir(__dirname)
+  await call({
+    cmd: "tsc",
+    args: ["-p", "src"],
+  })
+}
+
+main()
+
