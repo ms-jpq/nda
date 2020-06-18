b49cd1dfb2548a8a15eb206a55f1399984e2c4a0
Fly me to the moon <git@bigly.dog>
2020-06-18 13:03:13 -0700

    better big pring

--- src/node/console.ts
+++ src/node/console.ts
@@ -1,8 +1,7 @@
 export const big_print = (str: string, top = " ", btm = " ") => {
   const { columns } = process.stdout
-  return `
-  ${top.repeat(columns - 2)}
-  ${str}
-  ${btm.repeat(columns - 2)}
-  `
+  const t = top.repeat(columns - 2)
+  const b = btm.repeat(columns - 2)
+  return `${t}\n${str}\n${b}`
 }
+
