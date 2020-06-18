b711c6161fa9beafcaf782fdb6c1293f507f9be4
Fly me to the moon <git@bigly.dog>
2020-06-17 17:33:15 -0700

    added long zip

--- src/isomorphic/iterator.ts
+++ src/isomorphic/iterator.ts
@@ -1,6 +1,6 @@
 import { str } from "./prelude"
 
-export const range = function*(begin: number, end: number, step = 1) {
+export const range = function* (begin: number, end: number, step = 1) {
   let nxt = begin
   const cmp =
     step > 0
@@ -12,13 +12,13 @@ export const range = function*(begin: number, end: number, step = 1) {
   }
 }
 
-export const generate = function*<T>(gen: () => T, n: number) {
+export const generate = function* <T>(gen: () => T, n: number) {
   for (const _ of range(1, n)) {
     yield gen()
   }
 }
 
-export const enumerate = function*<T>(
+export const enumerate = function* <T>(
   iterable: Iterable<T>,
 ): Generator<[number, T]> {
   let idx = 0
@@ -28,7 +28,7 @@ export const enumerate = function*<T>(
   }
 }
 
-export const take = function*<T>(n: number, iterable: Iterable<T>) {
+export const take = function* <T>(n: number, iterable: Iterable<T>) {
   for (const [idx, el] of enumerate(iterable)) {
     if (idx >= n) {
       break
@@ -37,7 +37,7 @@ export const take = function*<T>(n: number, iterable: Iterable<T>) {
   }
 }
 
-export const drop = function*<T>(n: number, iterable: Iterable<T>) {
+export const drop = function* <T>(n: number, iterable: Iterable<T>) {
   for (const [idx, el] of enumerate(iterable)) {
     if (idx >= n) {
       yield el
@@ -45,13 +45,13 @@ export const drop = function*<T>(n: number, iterable: Iterable<T>) {
   }
 }
 
-export const map = function*<T, U>(trans: (_: T) => U, iterable: Iterable<T>) {
+export const map = function* <T, U>(trans: (_: T) => U, iterable: Iterable<T>) {
   for (const el of iterable) {
     yield trans(el)
   }
 }
 
-export const flat_map = function*<T, U>(
+export const flat_map = function* <T, U>(
   trans: (_: T) => Iterable<U>,
   iterable: Iterable<T>,
 ) {
@@ -60,7 +60,7 @@ export const flat_map = function*<T, U>(
   }
 }
 
-export const compact_map = function*<T, U>(
+export const compact_map = function* <T, U>(
   trans: (_: T) => U | undefined,
   iterable: Iterable<T>,
 ) {
@@ -72,7 +72,7 @@ export const compact_map = function*<T, U>(
   }
 }
 
-export const filter = function*<T>(
+export const filter = function* <T>(
   predicate: (_: T) => boolean,
   iterable: Iterable<T>,
 ) {
@@ -121,7 +121,7 @@ export const has = <T>(predicate: (_: T) => boolean, iterable: Iterable<T>) => {
   return false
 }
 
-export const zip = function*<T, U>(
+export const zip = function* <T, U>(
   iterable1: Iterable<T>,
   iterable2: Iterable<U>,
 ): Generator<[T, U]> {
@@ -139,7 +139,25 @@ export const zip = function*<T, U>(
   }
 }
 
-export const interlace = function*<T>(e: T, iterable: Iterable<T>) {
+export const longzip = function* <T, U>(
+  iterable1: Iterable<T>,
+  iterable2: Iterable<U>,
+): Generator<[T, U]> {
+  const [iter1, iter2] = [
+    iterable1[Symbol.iterator](),
+    iterable2[Symbol.iterator](),
+  ]
+  while (true) {
+    const [r1, r2] = [iter1.next(), iter2.next()]
+    if (r1.done && r2.done) {
+      break
+    } else {
+      yield [r1.value, r2.value]
+    }
+  }
+}
+
+export const interlace = function* <T>(e: T, iterable: Iterable<T>) {
   let fst = true
   for (const el of iterable) {
     if (!fst) {
@@ -202,7 +220,7 @@ export const sort_by_keys = <T>(
   return [...iterable].sort(sort)
 }
 
-export const unique_by = function*<T, U>(
+export const unique_by = function* <T, U>(
   key_by: (_: T) => U,
   iterable: Iterable<T>,
 ) {
@@ -216,7 +234,7 @@ export const unique_by = function*<T, U>(
   }
 }
 
-export const chunk = function*<T>(size: number, iterable: Iterable<T>) {
+export const chunk = function* <T>(size: number, iterable: Iterable<T>) {
   let coll: T[] = []
   for (const [idx, el] of enumerate(iterable)) {
     if (idx % size === 0 && idx !== 0) {
@@ -235,3 +253,4 @@ export const join = <T>(sep: string, iterable: Iterable<T>) => {
   }
   return s
 }
+
