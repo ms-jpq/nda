8d4b34bab5ab180f4a68e905cb3d4cb2a2e7f99d
IIIIII <git@bigly.dog>
2022-08-28 18:31:07 +0000

    name

--- build.mjs
+++ build.mjs
@@ -2,7 +2,7 @@
 import { spawnSync } from "node:child_process"
 import { dirname } from "node:path"
 
-const top_level = dirname(new URL(import.meta.url).pathname)
+const top_level = dirname(fileURLToPath(new URL(import.meta.url)))
 
 const { error, status } = spawnSync("tsc", ["--project", top_level], {
   stdio: "inherit",
