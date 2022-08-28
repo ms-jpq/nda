0a22e20321b3addc2e0b5c91d6678facd0501735
IIIIII <git@bigly.dog>
2022-08-28 18:42:50 +0000

    typo

--- build.mjs
+++ build.mjs
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { spawnSync } from "node:child_process"
 import { dirname } from "node:path"
+import { fileURLToPath } from "node:url"
 
 const top_level = dirname(fileURLToPath(new URL(import.meta.url)))
 
