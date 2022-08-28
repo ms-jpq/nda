bcca49606566ee99d0d29d177af02b73ea82c8b2
IIIIII <git@bigly.dog>
2022-08-28 19:26:09 +0000

    kk

--- src/node/console.ts
+++ src/node/console.ts
@@ -1,8 +1,11 @@
+import { assert } from "../isomorphic/assertion.js"
+import { EOL } from "node:os"
 import { stdout } from "node:process"
 
 export const big_print = (str: string, top = " ", btm = " ") => {
+  assert(top.length == 1 && btm.length == 1)
   const { columns } = stdout
   const t = top.repeat(columns)
   const b = btm.repeat(columns)
-  return `${t}\n${str}\n${b}`
+  return `${t}${EOL}${str}${EOL}${b}`
 }
