d40373e79e8176c961da0521aeb62e17134386a3
IIIIII <github@bigly.dog>
2024-08-26 00:12:22 -0400

    parse

---
+++ src/iso/json.ts
@@ -0,0 +1,18 @@
+export const loads = <T = unknown>(json: string) =>
+  JSON.parse(json, (_, v) =>
+    v !== null && typeof v === "object"
+      ? Object.assign(Object.create(null), v)
+      : v,
+  ) as T
+
+export const dumps = (value: unknown, space?: number) =>
+  JSON.stringify(
+    value,
+    (_, v) =>
+      v instanceof Map
+        ? Object.fromEntries(v)
+        : v instanceof Set
+          ? Array.from(v)
+          : v,
+    space,
+  )
